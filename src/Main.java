24c16dd6fa0bca171e223698967ec16975734a2f
Josh Lewis <josh.rb.lewis@gmail.com>
2021-03-17 20:34:05 -0600

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args)
     {
-
+        File InputFile = new File(args[0]);
+        Scanner readFile = new Scanner(InputFile);
+        textProcess(readFile);
 
     }
 
