3d6d1918348a3547cad648bf1ce301d995456bd8
Josh Lewis <josh.rb.lewis@gmail.com>
2021-03-29 16:55:10 -0600

    getters for Game

--- src/Game.java
+++ src/Game.java
@@ -10,4 +10,34 @@ private String EU_sales;
 private String JP_sales;
 private String other_sales;
 private String global_sales;
+public String getName() {
+    return name;
+}
+public String getPlatform() {
+    return platform;
+}
+public String getYear() {
+    return year;
+}
+public String getGenre() {
+    return genre;
+}
+public String getPublisher() {
+    return publisher;
+}
+public String getNA_sales() {
+    return NA_sales;
+}
+public String getEU_sales() {
+    return EU_sales;
+}
+public String getJP_sales() {
+    return JP_sales;
+}
+public String getOther_sales() {
+    return other_sales;
+}
+public String getGlobal_sales() {
+    return global_sales;
+}
 }
