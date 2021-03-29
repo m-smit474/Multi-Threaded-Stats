9af51402fce92bb964c44e76c9c5f4ee986a472e
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2021-03-29 16:52:03 -0600

    Create Game.java

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+
+public class Game {
+private String name;
+private String platform;
+private String year;
+private String genre;
+private String publisher;
+private String NA_sales;
+private String EU_sales;
+private String JP_sales;
+private String other_sales;
+private String global_sales;
+}
